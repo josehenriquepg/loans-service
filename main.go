c758596cc2dedc1a6574a949211a738c8e432368
josehenriquepg <henricopg11@gmail.com>
2024-06-14 17:19:29 -0300

    feat: data structure

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+type CustomerRequest struct {
+	Age      int     `json:"age"`
+	CPF      string  `json:"cpf"`
+	Name     string  `json:"name"`
+	Income   float64 `json:"income"`
+	Location string  `json:"location"`
+}
+
+type LoanDetail struct {
+	Type         string  `json:"type"`
+	InterestRate float64 `json:"interest_rate"`
+}
+
+type LoanResponse struct {
+	Customer string       `json:"customer"`
+	Loans    []LoanDetail `json:"loans"`
+}
