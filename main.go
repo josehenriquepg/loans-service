7f4941c0f18848b8889101d64863e110f8566b26
josehenriquepg <henricopg11@gmail.com>
2024-06-15 15:54:38 -0300

    feat: Function that determines the types of loans available

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strings"
+)
+
+// Structure that represents the customer's request
 type CustomerRequest struct {
 	Age      int     `json:"age"`
 	CPF      string  `json:"cpf"`
@@ -8,12 +13,40 @@ type CustomerRequest struct {
 	Location string  `json:"location"`
 }
 
+// Structure representing a loan option in the response
 type LoanDetail struct {
 	Type         string  `json:"type"`
 	InterestRate float64 `json:"interest_rate"`
 }
 
+// Structure that represents the response to the customer
 type LoanResponse struct {
 	Customer string       `json:"customer"`
 	Loans    []LoanDetail `json:"loans"`
+}
+
+// Function that determines the types of loans available
+func getAvailableLoans(c CustomerRequest) []LoanDetail {
+	var loans []LoanDetail
+
+	// Personal loan
+	if c.Income <= 3000 {
+		loans = append(loans, LoanDetail{Type: "Pessoal", InterestRate: 4})
+	} else if c.Income > 3000 && c.Income <= 5000 && c.Age < 30 && strings.EqualFold(c.Location, "SP") {
+		loans = append(loans, LoanDetail{Type: "PERSONAL", InterestRate: 4})
+	}
+
+	// Consignment loan
+	if c.Income >= 5000 {
+		loans = append(loans, LoanDetail{Type: "CONSIGNMENT", InterestRate: 2})
+	}
+
+	// Guaranteed loan
+	if c.Income <= 3000 {
+		loans = append(loans, LoanDetail{Type: "GUARANTEED", InterestRate: 3})
+	} else if c.Income > 3000 && c.Income <= 5000 && c.Age < 30 && strings.EqualFold(c.Location, "SP") {
+		loans = append(loans, LoanDetail{Type: "GUARANTEED", InterestRate: 3})
+	}
+
+	return loans
 }
